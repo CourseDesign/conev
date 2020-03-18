8e90568d9c4aa0e9a73f7ecfda5ea63a93863a99
박시율 <kdpark0723@gmail.com>
2020-03-18 21:07:43 +0900

    fix error

--- lib/source/add-source.ts
+++ lib/source/add-source.ts
@@ -3,11 +3,7 @@ import { Source } from './source';
 
 function addSource(sources: Array<Source>, source: Source, priority: number = -1) {
   if (priority === -1) sources.push(source);
-  else {
-    const moveSources = sources.slice(priority, sources.length);
-    sources.push(source);
-    sources.push(...moveSources);
-  }
+  else sources.splice(priority, 0, source);
 }
 
 export = addSource;
